0195bb3364b6d87d33eb4a37387bd7156e011c66
ogjex <m.agnu.ha@gmail.com>
2025-11-14 14:16:11 +0100

    update to working terraform, provider and resources

---
+++ tf-ruc/production.tf
@@ -0,0 +1,31 @@
+resource "proxmox_vm_qemu" "production" {
+    vmid = 200
+    name        = "debian-production"
+    target_node = "pve"
+
+    clone = "debian13"
+    full_clone = false
+    bios = "ovmf"
+    agent = 1
+    scsihw = "virtio-scsi-single"
+
+    os_type = "debian"
+    cpu {
+	type = "x86-64-v2-AES"
+	sockets = 1
+	cores = 1
+    }
+    memory = 2048
+
+    disks {
+	scsi {
+	    scsi0 {
+		disk {
+		    size = "6G"
+		    storage = "local"
+		    format = "qcow2"
+    }
+	}
+	    }
+		}
+}
