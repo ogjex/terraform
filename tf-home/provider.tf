b574cd3f7ec8095594ded37858258549601c6e46
ogjex <m.agnu.ha@gmail.com>
2025-12-16 14:59:12 +0100

    updated credentials and providers and production

---
+++ tf-home/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.2-rc05"
+    }
+  }
+}
+
+variable proxmox_api_url {
+    type = string
+}
+variable proxmox_api_token_id {
+    type = string
+}
+variable proxmox_api_token {
+    type = string
+}
+
+provider "proxmox" {
+  # Configuration options
+    pm_api_url = var.proxmox_api_url
+    pm_api_token_id = var.proxmox_api_token_id
+    pm_api_token_secret = var.proxmox_api_token
+    pm_tls_insecure = true
+    pm_log_enable = true
+    pm_log_file = "terraform-plugin-proxmox.log"
+    pm_debug = true
+    pm_log_levels = {
+    _default = "debug"
+    _capturelog = ""
+   }
+}
